c96b8c8ff57163fa93ee4e90bcc934c22020a9e7
Willanov <willanov@yandex.ru>
2023-06-18 14:15:35 +0500

    Added First

---
+++ First/Program.cs
@@ -0,0 +1,24 @@
+﻿int Prompt(string message)
+{
+    Console.WriteLine(message);
+    int a = int.Parse(Console.ReadLine()); 
+    return a;
+}
+void PromptMass(int M)
+{
+    int count = 0;
+    int[] a = new int[M];
+    for (int i=0;i<M;i++)
+    {
+        Console.WriteLine($"Введите элемент под номером {i}:");
+        int temp=Convert.ToInt32(Console.ReadLine());
+        if (temp>0){count++;}
+        a[i]=temp;
+    }
+    Console.WriteLine($"Всего чисел больше нуля {count}");
+
+
+}
+Console.Clear();
+int M = Prompt("Введите М:");
+PromptMass(M);
